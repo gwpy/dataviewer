9bb373e01f54884c9e5656b13899f274e2ee6d6d
mikelovskij <snao20@hotmail.com>
2015-09-18 21:44:56 +0200

    Removed the root-squaring and the subsequent squaring of the evaluated spectrograms, adjusted some identation.

--- dataviewer/bnsrange.py
+++ dataviewer/bnsrange.py
@@ -24,11 +24,9 @@ from epics import caget
 import re
 from itertools import (cycle, izip_longest)
 
-
-
 from astropy.time import Time
 
-from gwpy.timeseries import (TimeSeries,TimeSeriesDict)
+from gwpy.timeseries import (TimeSeries, TimeSeriesDict)
 from gwpy.spectrogram import (Spectrogram, SpectrogramList)
 from gwpy.plotter import (SpectrogramPlot, TimeSeriesAxes)
 from gwpy.spectrum import Spectrum
@@ -38,7 +36,6 @@ import warnings
 
 warnings.filterwarnings("ignore")
 import pickle
-
 
 from . import version
 from .buffer import (OrderedDict, DataBuffer)
@@ -57,7 +54,8 @@ __all__ = ['BNSRangeSpectrogramMonitor']
 #
 # -----------------------------------------------------------------------------
 
-stateDQ = 1 # defines whether the data is meaningful or not (redefined below)
+stateDQ = 1  # defines whether the data is meaningful or not (redefined below)
+
 
 class SpectrogramBuffer(DataBuffer):
     DictClass = OrderedDict
@@ -100,31 +98,30 @@ class SpectrogramBuffer(DataBuffer):
         # calculate spectrograms only if state conditon(s) is satisfied
         data = self.DictClass()
         if stateDQ:
-          for channel, ts in zip(self.channels, tsd.values()):
-            try:
-                specgram = ts.spectrogram(stride[channel],
-                                          fftlength=fftlength[channel],
-                                          overlap=overlap[channel]) ** (1/2.)
-            except ZeroDivisionError:
-                if stride[channel] == 0:
-                    raise ZeroDivisionError("Spectrogram stride is 0")
-                elif fftlength[channel] == 0:
-                    raise ZeroDivisionError("FFT length is 0")
-                else:
-                    raise
-            if hasattr(channel, 'resample') and channel.resample is not None:
-                nyq = float(channel.resample) / 2.
-                nyqidx = int(nyq / specgram.df.value)
-                specgram = specgram[:,:nyqidx]
-            if channel in filter and filter[channel]:
-                specgram = specgram.filter(*filter[channel]).copy()
-            data[channel] = specgram
+            for channel, ts in zip(self.channels, tsd.values()):
+                try:
+                    specgram = ts.spectrogram(stride[channel],
+                                              fftlength=fftlength[channel],
+                                              overlap=overlap[channel])
+                except ZeroDivisionError:
+                    if stride[channel] == 0:
+                        raise ZeroDivisionError("Spectrogram stride is 0")
+                    elif fftlength[channel] == 0:
+                        raise ZeroDivisionError("FFT length is 0")
+                    else:
+                        raise
+                if hasattr(channel, 'resample') and channel.resample is not None:
+                    nyq = float(channel.resample) / 2.
+                    nyqidx = int(nyq / specgram.df.value)
+                    specgram = specgram[:, :nyqidx]
+                if channel in filter and filter[channel]:
+                    specgram = specgram.filter(*filter[channel]).copy()
+                data[channel] = specgram
 
         return data
 
 
 class SpectrogramIterator(SpectrogramBuffer):
-
     def _next(self):
         new = super(SpectrogramIterator, self)._next()
         return self.from_timeseriesdict(
@@ -141,13 +138,14 @@ class SpectrogramIterator(SpectrogramBuffer):
 class UserException(Exception):
     pass
 
+
 class BNSRangeSpectrogramMonitor(TimeSeriesMonitor):
     """Monitor some spectra
     """
     type = 'bnsrangespectrogram'
     FIGURE_CLASS = SpectrogramPlot
     AXES_CLASS = TimeSeriesAxes
- 
+
     def __init__(self, *channels, **kwargs):
         global stateDQ
         # get FFT parameters
@@ -200,16 +198,16 @@ class BNSRangeSpectrogramMonitor(TimeSeriesMonitor):
             stateChannels = self.stateChannel[0].split(",")
             stateCondition = self.stateChannel[1].split(",")
             for i, lockChanls in enumerate(stateChannels):
-                stateDQ = stateDQ and eval(str(caget(lockChanls))+stateCondition[i])
+                stateDQ = stateDQ and eval(str(caget(lockChanls)) + stateCondition[i])
         elif len(self.stateChannel) > 2:
             raise UserException("Unknown state channels/ conditions")
 
         super(BNSRangeSpectrogramMonitor, self).__init__(*channels,
-              **kwargs)
+                                                         **kwargs)
         self.buffer.channels = self.spectrograms.channels
 
         # reset buffer duration to store a single stride
-        #self.duration = self.buffer.duration
+        # self.duration = self.buffer.duration
         self.buffer.duration = kwargs['interval']
 
         if ratio is not None:
@@ -241,8 +239,8 @@ class BNSRangeSpectrogramMonitor(TimeSeriesMonitor):
 
     def init_figure(self):
         self._fig = self.FIGURE_CLASS(**self.params['figure'])
-        #sharex = None
-        if self.params['draw']['norm'][0]=="linear":
+        # sharex = None
+        if self.params['draw']['norm'][0] == "linear":
             self.params['draw']['norm'][0] = ''
 
         def _new_axes():
@@ -254,19 +252,19 @@ class BNSRangeSpectrogramMonitor(TimeSeriesMonitor):
 
         for n in range(len(self.plots)):
             _new_axes()
-            #sharex = _new_axes()
+            # sharex = _new_axes()
         yscale = self.params['init'].pop('yscale')
         self.set_params('init')
         for ax in self._fig.get_axes(self.AXES_CLASS.name)[:-1]:
             ax.set_xlabel('')
         for i, ax in enumerate(self._fig.get_axes(self.AXES_CLASS.name)):
             if isinstance(yscale, str):
-               yscalePlot = yscale
+                yscalePlot = yscale
             else:
-               yscalePlot = yscale[i]
+                yscalePlot = yscale[i]
             ax.set_yscale(yscalePlot)
         self.set_params('refresh')
-        #ax.set_xlim(float(self.epoch - self.duration), float(self.epoch))
+        # ax.set_xlim(float(self.epoch - self.duration), float(self.epoch))
         return self._fig
 
     def update_data(self, new, gap='pad', pad=0):
@@ -274,122 +272,126 @@ class BNSRangeSpectrogramMonitor(TimeSeriesMonitor):
         This method only applies a ratio, if configured
         """
         # data buffer will return dict of 1-item lists, so reform to tsd
-        pickleFile = 'rangefile' # to store data at each step
+        pickleFile = 'rangefile'  # to store data at each step
         new = TimeSeriesDict((key, val[0]) for key, val in new.iteritems())
-        self.epoch = new[self.channels[0]].epoch.gps+self.stride
+        self.epoch = new[self.channels[0]].epoch.gps + self.stride
         if not self.spectrograms.data:
             try:
                 pickleHandle = open(pickleFile, 'r')
                 tempSpect = pickle.load(pickleHandle)
                 pickleHandle.close()
                 self.spectrograms.data[self.channels[0]] = tempSpect
-            except: 
+            except:
                 pass
         self.spectrograms.append(self.spectrograms.from_timeseriesdict(new))
         self.spectrograms.crop(self.epoch - self.duration)
         self.data = type(self.spectrograms.data)()
         if self.spectrograms.data:
-          for channel in self.channels:
-            self.data[channel] = type(self.spectrograms.data[channel])()
-            for spec in self.spectrograms.data[channel]:
-                ranges = []
-                for x in spec:
-                    asd = Spectrum(x.value, frequencies=spec.frequencies, channel=spec.channel, unit=spec.unit).crop(self.flow, self.fhigh)
-                    range_spec = inspiral_range_psd(asd**2)
-                    ranges.append((range_spec*range_spec.df.value)**(0.5))
-                self.data[channel].append(type(spec).from_spectra(
-                         *ranges, epoch=spec.epoch, dt=spec.dt))
-          pickleHandle = open(pickleFile, 'w')
-          pickle.dump(self.spectrograms.data[channel],pickleHandle)
-          pickleHandle.close()        
+            for channel in self.channels:
+                self.data[channel] = type(self.spectrograms.data[channel])()
+                for spec in self.spectrograms.data[channel]:
+                    ranges = []
+                    for x in spec:
+                        psd = Spectrum(x.value, frequencies=spec.frequencies, channel=spec.channel,
+                                       unit=spec.unit).crop(self.flow, self.fhigh)
+                        range_spec = inspiral_range_psd(psd)
+                        ranges.append((range_spec * range_spec.df.value) ** (0.5))
+                    self.data[channel].append(type(spec).from_spectra(
+                        *ranges, epoch=spec.epoch, dt=spec.dt))
+            pickleHandle = open(pickleFile, 'w')
+            pickle.dump(self.spectrograms.data[channel], pickleHandle)
+            pickleHandle.close()
         return self.data
 
     def refresh(self):
-      # extract data
-      # replot all spectrogram
-      if self.data:
-      #if self.data[self.channels[0]]:
-        axes = cycle(self._fig.get_axes(self.AXES_CLASS.name))
-        coloraxes = self._fig.colorbars
-        params = self.params['draw']
-        if len(self.data.keys()) == 1: # if only one channel given then proceed
-            channel = self.data.keys()[0]
-        else:
-            raise UserException("Only one channel is accepted for BNSrange Monitor")
+        # extract data
+        # replot all spectrogram
+        if self.data:
+            # if self.data[self.channels[0]]:
+            axes = cycle(self._fig.get_axes(self.AXES_CLASS.name))
+            coloraxes = self._fig.colorbars
+            params = self.params['draw']
+            if len(self.data.keys()) == 1:  # if only one channel given then proceed
+                channel = self.data.keys()[0]
+            else:
+                raise UserException("Only one channel is accepted for BNSrange Monitor")
 
-        # plot spectrograms
-        newSpectrogram = self.data[channel]
-        for i, plotType in enumerate(self.plots):
-            ax = next(axes)
-            # plot new data
-            pparams = {}
-            for key in params:
-                try:
-                    if params[key][i]:
-                        pparams[key] = params[key][i]
-                except IndexError:
-                    pass
-            if plotType == "timeseries":
-                for spec in newSpectrogram:
-                   rangIntegrand = spec**2
-                   rangeTimeseriesSquare = rangIntegrand.sum(axis=1)
-                   rangeTimeseries = TimeSeries(rangeTimeseriesSquare.data**0.5,
-                       epoch=rangeTimeseriesSquare.epoch,
-                       name=rangeTimeseriesSquare.name,
-                       sample_rate=1.0/rangeTimeseriesSquare.dt.value,
-                       unit='Mpc', channel=rangeTimeseriesSquare.name)
-                   #coll = ax.plot(rangeTimeseries, label=label, color='b',
-                   coll = ax.plot(rangeTimeseries, color='b',
-                               linewidth=3.0,marker='o')
-            elif plotType == "spectrogram":  
-                #coll = ax.plot(newSpectrogram, label=label, **pparams)
-                for spec in newSpectrogram:
-                    coll = ax.plot(spec, **pparams)
-                try:
-                    coloraxes[i]
-                except IndexError:
-                    cbparams = {}
-                    for key in self.params['colorbar']:
-                        try:
-                            if self.params['colorbar'][key][i]:
-                                cbparams[key] = self.params['colorbar'][key][i]
-                        except IndexError:
-                            pass
+            # plot spectrograms
+            newSpectrogram = self.data[channel]
+            for i, plotType in enumerate(self.plots):
+                ax = next(axes)
+                # plot new data
+                pparams = {}
+                for key in params:
                     try:
-                        self._fig.add_colorbar(mappable=coll, ax=ax, 
-                                       **cbparams)
-                    except Exception as e:
-                        self.logger.error(str(e))
-            else:
-                raise UserException("Unknown plot option")
-            #label = None
-        k = 0 # for resizing plots to look better
-        for ax in self._fig.get_axes(self.AXES_CLASS.name):
-            if not k:
-                l, b, w, h = ax.get_position().bounds
-                k = 1
-            else:
-                ll, bb, ww, hh = ax.get_position().bounds
-                if w != ww:
-                     ax.set_position([ll, bb, w, h])
-            ax.relim()
-            #ax.autoscale_view(scalex=False)
+                        if params[key][i]:
+                            pparams[key] = params[key][i]
+                    except IndexError:
+                        pass
+                if plotType == "timeseries":
+                    for spec in newSpectrogram:
+                        rangIntegrand = spec ** 2
+                        rangeTimeseriesSquare = rangIntegrand.sum(axis=1)
+                        self.debug.info(
+                            'Estimated BNS range of {0}'.format(rangeTimeseriesSquare ** 0.5))
+                        rangeTimeseries = TimeSeries(rangeTimeseriesSquare.data ** 0.5,
+                                                     epoch=rangeTimeseriesSquare.epoch,
+                                                     name=rangeTimeseriesSquare.name,
+                                                     sample_rate=1.0 / rangeTimeseriesSquare.dt.value,
+                                                     unit='Mpc', channel=rangeTimeseriesSquare.name)
+                        # coll = ax.plot(rangeTimeseries, label=label, color='b',
+                        coll = ax.plot(rangeTimeseries, color='b',
+                                       linewidth=3.0, marker='o')
+                elif plotType == "spectrogram":
+                    # coll = ax.plot(newSpectrogram, label=label, **pparams)
+                    for spec in newSpectrogram:
+                        coll = ax.plot(spec, **pparams)
+                    try:
+                        coloraxes[i]
+                    except IndexError:
+                        cbparams = {}
+                        for key in self.params['colorbar']:
+                            try:
+                                if self.params['colorbar'][key][i]:
+                                    cbparams[key] = self.params['colorbar'][key][i]
+                            except IndexError:
+                                pass
+                        try:
+                            self._fig.add_colorbar(mappable=coll, ax=ax,
+                                                   **cbparams)
+                        except Exception as e:
+                            self.logger.error(str(e))
+                else:
+                    raise UserException("Unknown plot option")
+                    # label = None
+            k = 0  # for resizing plots to look better
+            for ax in self._fig.get_axes(self.AXES_CLASS.name):
+                if not k:
+                    l, b, w, h = ax.get_position().bounds
+                    k = 1
+                else:
+                    ll, bb, ww, hh = ax.get_position().bounds
+                    if w != ww:
+                        ax.set_position([ll, bb, w, h])
+                ax.relim()
+                # ax.autoscale_view(scalex=False)
 
-        self.logger.debug('Figure data updated')
-        # add suptitle
-        if not 'suptitle' in self.params['init']:
-            prefix = ('FFT length: %ss, Overlap: %ss, Stride: %ss -- '
-                      % (self.fftlength, self.overlap, self.stride))
-            utc = re.sub('\.0+', '',
-                         Time(self.epoch, format='gps', scale='utc').iso)
-            suffix = 'Last updated: %s UTC (%s)' % (utc, self.epoch)
-            self.suptitle = self._fig.suptitle(prefix + suffix)
-      for ax in self._fig.get_axes(self.AXES_CLASS.name):
-          ax.set_xlim(float(self.epoch - self.duration),
-                          float(self.epoch))
-          ax.set_epoch(self.epoch)
-      self.set_params('refresh')
-      self._fig.refresh()
-      self.logger.debug('Figure refreshed')
+            self.logger.debug('Figure data updated')
+            # add suptitle
+            if not 'suptitle' in self.params['init']:
+                prefix = ('FFT length: %ss, Overlap: %ss, Stride: %ss -- '
+                          % (self.fftlength, self.overlap, self.stride))
+                utc = re.sub('\.0+', '',
+                             Time(self.epoch, format='gps', scale='utc').iso)
+                suffix = 'Last updated: %s UTC (%s)' % (utc, self.epoch)
+                self.suptitle = self._fig.suptitle(prefix + suffix)
+        for ax in self._fig.get_axes(self.AXES_CLASS.name):
+            ax.set_xlim(float(self.epoch - self.duration),
+                        float(self.epoch))
+            ax.set_epoch(self.epoch)
+        self.set_params('refresh')
+        self._fig.refresh()
+        self.logger.debug('Figure refreshed')
+
 
 register_monitor(BNSRangeSpectrogramMonitor)
